9b1ea7e068b97045dce98e1e0bdadd418d1c3f63
AytajMammadova306 <aytajamam@code.edu.az>
2025-07-30 15:56:32 +0400

    initial

---
+++ homework6/homework6/Program.cs
@@ -0,0 +1,33 @@
+﻿namespace homework6
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int s=Area(5,3,4,1);
+            Console.WriteLine(s);
+        }
+        public static int Area (int r)
+        {
+            int p = 3;
+            int s = p * r * r;
+            return s;
+        }
+        public static int Area(int a, int b)
+        {
+            int s = a * b;
+            return s;
+        }
+        public static int Area(int a, int b, int c)
+        {
+            int s = 2 * (a * b + a * c + b * c);
+            return s;
+        }
+        public static int Area(int a, int b, int c, int r)
+        {
+            int p = (a + b + c) / 2;
+            int s= p * r;
+            return s;
+        }
+    }
+}
